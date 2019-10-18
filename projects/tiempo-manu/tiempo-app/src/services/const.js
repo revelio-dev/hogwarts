2bc6bf3ae7d38b5543ec2d5f2fc747184f1e385c
CM-CA <manuel.couseloalonso@gmail.com>
2019-10-18 16:20:49 +0200

    fixing const apikey and url

--- projects/tiempo-manu/tiempo-app/src/services/const.js
+++ projects/tiempo-manu/tiempo-app/src/services/const.js
@@ -1,13 +1,5 @@
 
-/*import Vue from 'vue'
-
-const API_KEY = Vue.config.app.VUE_APP_API_KEY;
-const URL = Vue.config.app.VUE_APP_API_ROOT;*/
-
-//Haciendo lo de arriba, me dice que las variables de entorno, no estn definidas.. y la verdad, no entiendo el
-// bien el porque.
-
-const API_KEY = "aqui el ap_key"
-const URL = "ruta url de la api"
+const API_KEY = process.env.VUE_APP_API_KEY;
+const URL = process.env.VUE_APP_API_ROOT;
 
 export { API_KEY, URL }
