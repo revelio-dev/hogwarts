0f199270e50f7286073032dc53ca329c651d4444
Carlacid <carlacidf98@gmail.com>
2019-10-18 16:03:46 +0200

    improve text sobre mi

--- projects/tiempoCarlaSergio/src/views/Sobremi.vue
+++ projects/tiempoCarlaSergio/src/views/Sobremi.vue
@@ -1,6 +1,16 @@
 <template>
-  <div id="sobremi" class="mt-2">
-    <p>Página a nivel nacional que muestra el tiempo</p>
+  <div id="sobremi" class="mt-2 text-justify">
+    <p>La información meteorológica elaborada a partir de la actividad científica
+      y técnica desenvolvida por la Conserjería de Medio Ambiente, Territorio y Vivienda,
+      Tiempo, acorde con el prograso científico y tecnológico es objeto de propiedades intelectuales
+      y goza de la protección reconocida por Texto Refundido de la Lei de Propiedad Intelectual aprobado
+      por Real Decreto Legislativo 1/1996 de 12 de abril.Protección de datos</p>
+      <div class="text-center">
+        <p class="font-weight-bold">TIEMPO &copy;</p>
+        <p> <span class="font-italic">Teléfono:</span> (+34) 881 999 654</p>
+        <p><span class="font-italic">Email:</span> tiempo@tiempo.es</p>
+        <p>Conserjería de Medio Ambiente, Territorio y Vivienda</p>
+      </div>
   </div>
 </template>
 
