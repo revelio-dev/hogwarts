2e853f27d34159d16fbcaa15139bbef18067c38c
CM-CA <manuel.couseloalonso@gmail.com>
2019-10-20 21:01:57 +0200

    rearrenge code, need components

---
+++ projects/tiempo-manu/tiempo-app/src/components/Weather-Component/Imageweather.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "Imageweather"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- projects/tiempo-manu/tiempo-app/src/service/const.js
+++ projects/tiempo-manu/tiempo-app/src/service/const.js
@@ -1,4 +1,3 @@
-
 const API_KEY = process.env.VUE_APP_API_KEY;
 const URL = process.env.VUE_APP_API_ROOT;
 
