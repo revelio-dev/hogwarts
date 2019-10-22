4d044c7e581c365d31189c825b03d5c4686ba7d5
Camilo Couselo <manuel.couseloalonso@gmail.com>
2019-10-22 13:22:46 +0200

    adding weather-card component

---
+++ src/components/Weather-Component/Cardweather.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="cardweather">
+      <div class="container-fluid">
+          <b-card>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
